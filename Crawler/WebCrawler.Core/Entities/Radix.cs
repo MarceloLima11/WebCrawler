708208aad492a2d47f2fc1f5b17d0c80f06ceeb5
MarceloLima11 <marcelolima152021@gmail.com>
2024-01-15 14:49:20 -0300

    feat: radix path class

---
+++ Crawler/WebCrawler.Core/Entities/Radix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WebCrawler.Core.Entities
+{
+    class Radix
+    {
+        private string path;
+
+        public Radix(string path)
+        {
+            this.path = IsValidUrl(path) ? path: throw new ArgumentException("URL is invalid or not an HTTP/HTTPS URL.");
+        }
+
+        static bool IsValidUrl(string url)
+        {
+            return Uri.TryCreate(url, UriKind.Absolute, out var resultUri)
+                && (resultUri.Scheme == Uri.UriSchemeHttp || resultUri.Scheme == Uri.UriSchemeHttps);
+        }
+    }
+}
